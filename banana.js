3a4ba60396ad6dd93e8a88766826bd63b0e1cf0a
Joseph Moroney <josephmoroney97@gmail.com>
2021-12-24 18:46:25 +1000

    Added content config + scroll compat

--- banana.js
+++ banana.js
@@ -3,30 +3,79 @@ g.clear();
 const vw = g.getWidth();
 const vh = g.getHeight();
 
+let data = require("Storage").readJSON("banana-count") || [];
+
 const screenConfig = [
   {
     colour: "#CAFFBF",
+    layout: [
+      {
+        text: "TOTAL",
+        font: "6x8:3x3",
+        x: vw / 2,
+        y: 30,
+      },
+      {
+        text: data.length,
+        font: "6x8:9x9",
+        x: vw / 2,
+        y: vh / 2,
+      },
+      {
+        text: "eaten",
+        font: "6x8:3x3",
+        x: 110,
+        y: 150,
+      },
+    ],
   },
   {
     colour: "#9BF6FF",
+    layout: [
+      {
+        text: "GUNK",
+        font: "6x8:7x7",
+        x: vw / 2,
+        y: vh / 2,
+      },
+    ],
   },
   {
     colour: "#BDB2FF",
+    layout: [],
   },
   {
     colour: "#FFC6FF",
+    layout: [],
   },
 ];
 
+const hexToRgb = (hex) => {
+  return hex
+    .replace("#", "")
+    .match(/.{1,2}/g)
+    .map((rgb) => parseInt(rgb, 16));
+};
+
 const swipePercentage = 0.3; // how far a user must swipe to get to next/prev screen
 let touchPoint = null;
-let currentScreen = 1;
+let currentScreen = 0;
 
 g.setColor(screenConfig[currentScreen].colour);
 g.fillRect(0, 0, vw, vh);
 
+// Render screen content
+g.setColor("#333333");
+g.setFontAlign(0, 0);
+
+screenConfig[currentScreen].layout.forEach((item) => {
+  g.setFont(item.font);
+  g.drawString(item.text, item.x, item.y);
+});
+
 // @dir: "next" | "prev"
 const drawScreens = (dir, distance) => {
+  // Background
   g.setColor(
     screenConfig[dir === "prev" ? currentScreen - 1 : currentScreen].colour
   );
@@ -36,6 +85,22 @@ const drawScreens = (dir, distance) => {
     screenConfig[dir === "prev" ? currentScreen : currentScreen + 1].colour
   );
   g.fillRect(distance, 0, vw, vh);
+
+  // Content
+  g.setColor("#333333");
+  screenConfig[
+    dir === "prev" ? currentScreen - 1 : currentScreen
+  ].layout.forEach((item) => {
+    g.setFont(item.font);
+    g.drawString(item.text, item.x + distance - vw, item.y);
+  });
+
+  screenConfig[
+    dir === "prev" ? currentScreen : currentScreen + 1
+  ].layout.forEach((item) => {
+    g.setFont(item.font);
+    g.drawString(item.text, item.x + distance, item.y);
+  });
 };
 
 const handleDrag = (e) => {
